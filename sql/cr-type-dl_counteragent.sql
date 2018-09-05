ce8035026c9c3d53b190963737145949ab41d52b
Vladimir Scherbo <vscherbo@gmail.com>
2018-09-05 11:17:54 +0300

    db counteragents updated

--- sql/cr-type-dl_counteragent.sql
+++ sql/cr-type-dl_counteragent.sql
@@ -1,10 +1,14 @@
-drop type dl_counteragent;
-create type dl_counteragent as (
+drop type shp.dl_counteragent;
+create type shp.dl_counteragent as (
 "id" integer,
+"uid" UUID,
 "lastUpdate" timestamp,
 "name" text,
 "form" text,
 "type" text,
-"inn" text
--- "addresses" integer
+"inn" text,
+"addresses" integer,
+"Email" text,
+"Phone" text,
+"document" json
 );
--- sql/cr-vw_dl_counteragents.sql
+++ sql/cr-vw_dl_counteragents.sql
@@ -1,5 +1,5 @@
 /*
-create type dl_counteragent as (
+create type shp.dl_counteragent as (
 "lastUpdate" timestamp,
 name text,
 form text,
@@ -7,5 +7,13 @@ type text,
 inn text
 );*/
 
-create view vw_dl_counteragents as
-select (jsonb_populate_record(null::dl_counteragent, jsonb_array_elements(values)->'counteragent')).* from dl_counteragents_json
+drop view shp.vw_dl_counteragents;
+create view shp.vw_dl_counteragents as
+select *,
+document->'type' as dl_doc_type,
+document->'serial' as dl_doc_serial,
+document->'number' as dl_doc_number,
+document->'date'  as dl_doc_date
+from 
+(select (jsonb_populate_record(null::shp.dl_counteragent, jsonb_array_elements(values)->'counteragent')).* from shp.dl_counteragents_json) cnta
+-- select (jsonb_populate_record(null::shp.dl_counteragent, jsonb_array_elements(values)->'counteragent')).* from shp.dl_counteragents_json
