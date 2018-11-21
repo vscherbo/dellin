e99df07c48e4d5f1f8abbf8a788cad880087f0e1
Vladimir Scherbo <vscherbo@gmail.com>
2018-11-21 11:35:13 +0300

    varchar for VBA; schema "ext"

--- sql/cr-vw_dl_counteragents.sql
+++ sql/cr-vw_dl_counteragents.sql
@@ -1,13 +1,21 @@
-/*
-create type shp.dl_counteragent as (
-"lastUpdate" timestamp,
-name text,
-form text,
-type text,
-inn text
-);*/
+-- drop view shp.vw_dl_counteragents;
 
-drop view shp.vw_dl_counteragents;
+-- recreate type
+drop type shp.dl_counteragent cascade;
+create type shp.dl_counteragent as (
+"id" integer,
+"uid" UUID,
+"lastUpdate" timestamp,
+"name" varchar,
+"form" varchar,
+"type" varchar,
+"inn" varchar,
+"addresses" integer,
+"Email" varchar,
+"Phone" varchar,
+"document" json
+);
+
 create view shp.vw_dl_counteragents as
 select *,
 document->'type' as dl_doc_type,
@@ -15,5 +23,7 @@ document->'serial' as dl_doc_serial,
 document->'number' as dl_doc_number,
 document->'date'  as dl_doc_date
 from 
-(select (jsonb_populate_record(null::shp.dl_counteragent, jsonb_array_elements(values)->'counteragent')).* from shp.dl_counteragents_json) cnta
--- select (jsonb_populate_record(null::shp.dl_counteragent, jsonb_array_elements(values)->'counteragent')).* from shp.dl_counteragents_json
+(select (jsonb_populate_record(null::shp.dl_counteragent, jsonb_array_elements(values)->'counteragent')).* from ext.dl_counteragents_json) cnta;
+
+\i cr-vw_dl_addr_contact_ext.sql
+\i cr-vw_dl_addr_phone_ext.sql
