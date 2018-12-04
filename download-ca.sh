7c6779d3e50f4ec416f9994355fdf01fe497fec6
Vladimir Scherbo <vscherbo@gmail.com>
2018-12-04 12:04:12 +0300

    download counteragents dellin

---
+++ download-ca.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+. /usr/local/bin/bashlib
+
+WRKDIR=/opt/dellin
+cd $WRKDIR
+LOG=`namename $0`.log
+
+exec 1>$LOG 2>&1
+# Обновить справочник контрагентов
+#    на выходе файл res-counteragents.txt
+./get-dl-ca.py --log_level=INFO
+rc=$?
+# check ret_code , output res-counteragents.txt
+if [ $rc -eq 0 ]
+then
+    # выполняем на PG, параметр -h vm-pg опускаем 
+    psql --echo-all --variable ON_ERROR_STOP=ON -U arc_energo -f sql/copy-dl-counteragents.sql
+    rc=$?
+fi
+
+exit $rc
---
+++ get-dl-ca.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+# import os
+import sys
+
+import dl_app
+
+args = dl_app.parser.parse_args()
+ret_code = -1 
+
+app = dl_app.DL_app(args=args, description='DL counteragents in JSON download')
+logging.info("args={}".format(args))
+
+if app.login(auth=True):
+    dl_res = app.dl.dl_book_counteragents()
+    if 200 == app.dl.status_code:
+        logging.info('res_count={}'.format(1 + len(dl_res)))
+        with open('res-counteragents.txt', 'w') as of:
+            of.write(app.dl.text.replace('\n', ''))
+        ret_code = 0
+        """
+        load into PG with copy-dl-counteragents.sql
+        """
+    else:
+        err_str = 'ERROR={}'.format(app.dl.err_msg)
+        logging.error(err_str)
+        ret_code = 1
+
+    app.logout()
+else:    
+    ret_code = 1
+
+sys.exit(ret_code)
