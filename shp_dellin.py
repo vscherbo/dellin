a29e5fdb99dde9cd9cbac037c0a9a64a7f7d7cbe
Vladimir Scherbo <vscherbo@gmail.com>
2019-01-23 12:20:09 +0300

    PEP

--- shp_dellin.py
+++ shp_dellin.py
@@ -15,27 +15,34 @@ class DellinAPI(object):
     Base class for api.dellin.ru
     """
     host = "https://api.dellin.ru"
-    url_login = '{}/v1/customers/login.json'.format(host)
-    url_sfrequest = '{}/v1/customers/sfrequest.json'.format(host)
-    url_tracker = '{}/v1/public/tracker.json'.format(host)
-    url_tracker_adv = '{}/v1/public/tracker_advanced.json'.format(host)
-    url_calculator = '{}/v1/public/calculator.json'.format(host)
-    url_logout = '{}/v1/customers/logout.json'.format(host)
-    url_orders = '{}/v2/customers/orders.json'.format(host)
-    url_book_counteragents = '{}/v1/customers/book/counteragents.json'.format(host)  # справочник контрагентов
-    url_counteragents = '{}/v1/customers/counteragents.json'.format(host)  # наши фирмы, подключённые к ЛК
-    url_addresses = '{}/v1/customers/book/addresses.json'.format(host)  # список адресов контрагента
-    url_dir_countries = '{}/v1/public/countries.json'.format(host)
-    url_dir_opf_list = '{}/v1/public/opf_list.json'.format(host)
-    url_dir_places = '{}/v1/public/places.json'.format(host)
-    url_dir_streets = '{}/v1/public/streets.json'.format(host)
-    url_book_address = '{}/v1/customers/book/address.json'.format(host)  # Получение списка контактных лиц и телефонов
-    url_book_counteragents_update = '{}/v1/customers/book/counteragents/update.json'.format(host)
-    url_phones_update = '{}/v1/customers/book/phones/update.json'.format(host)  # добавить или обновить телефон по адресу
-    url_contacts_update = '{}/v1/customers/book/contacts/update.json'.format(host)  # добавить или обновить контакт по адресу
-    url_addresses_update = '{}/v1/customers/book/addresses/update.json'.format(host)  # добавить или обновить адрес контрагента
-    url_request = '{}/v1/customers/request.json'.format(host)
-    url_request_v2 = '{}/v2/request.json'.format(host)
+    url_login = '%s/v1/customers/login.json' % host
+    url_sfrequest = '%s/v1/customers/sfrequest.json' % host
+    url_tracker = '%s/v1/public/tracker.json' % host
+    url_tracker_adv = '%s/v1/public/tracker_advanced.json' % host
+    url_calculator = '%s/v1/public/calculator.json' % host
+    url_logout = '%s/v1/customers/logout.json' % host
+    url_orders = '%s/v2/customers/orders.json' % host
+    # справочник контрагентов
+    url_book_counteragents = '%s/v1/customers/book/counteragents.json' % host
+    # наши фирмы, подключённые к ЛК
+    url_counteragents = '%s/v1/customers/counteragents.json' % host
+    # список адресов контрагента
+    url_addresses = '%s/v1/customers/book/addresses.json' % host
+    url_dir_countries = '%s/v1/public/countries.json' % host
+    url_dir_opf_list = '%s/v1/public/opf_list.json' % host
+    url_dir_places = '%s/v1/public/places.json' % host
+    url_dir_streets = '%s/v1/public/streets.json' % host
+    # Получение списка контактных лиц и телефонов
+    url_book_address = '%s/v1/customers/book/address.json' % host
+    url_book_counteragents_update = '%s/v1/customers/book/counteragents/update.json' % host
+    # добавить или обновить телефон по адресу
+    url_phones_update = '%s/v1/customers/book/phones/update.json' % host
+    # добавить или обновить контакт по адресу
+    url_contacts_update = '%s/v1/customers/book/contacts/update.json' % host
+    # добавить или обновить адрес контрагента
+    url_addresses_update = '%s/v1/customers/book/addresses/update.json' % host
+    url_request = '%s/v1/customers/request.json' % host
+    url_request_v2 = '%s/v2/request.json' % host
     # headers = {'Content-type': 'application/javascript'}
     headers = {'Content-type': 'application/json'}
 
@@ -46,6 +53,7 @@ class DellinAPI(object):
         self.payload = {}
         self.text = ''
         self.status_code = 200
+        self.err_msg = None
 
         if login and password:
             assert isinstance(login, str)
@@ -76,39 +84,44 @@ class DellinAPI(object):
         return '{0} msg={1}'.format(tag, str(exception).encode('utf-8'))
 
     def dl_post(self, post_url):
+        """ POST an request to api.dellin.ru
+            Args:
+                post_url - URL on api.dellin.ru
+        """
         self.payload['appKey'] = self.app_key
         ret = None
         resp = None
+        loc_data = json.dumps(self.payload, ensure_ascii=False)
+        logging.debug("url=%s, data=%s", post_url, re.sub(
+            r"(.password.\s*:\s*)([\'\"].*?[\'\"])(.*?$)", r"\1 ***** \3"
+            , loc_data))
         try:
-            loc_data = json.dumps(self.payload, ensure_ascii=False)
-            logging.debug("url={}, data={}".format(post_url, re.sub(r"(.password.\s*:\s*)([\'\"].*?[\'\"])(.*?$)", r"\1 ***** \3", loc_data)))
-
-            resp = requests.post(post_url, json=self.payload, headers=self.headers)
+            resp = requests.post(post_url,
+                                 json=self.payload,
+                                 headers=self.headers)
             self.status_code = resp.status_code
             self.err_msg = None
-            logging.debug("status_code={}".format(resp.status_code))
-            # logging.debug("resp.headers={}".format(resp.headers))
-            # logging.debug("resp.url={}".format(resp.url))
+            logging.debug("status_code=%s", resp.status_code)
             resp.raise_for_status()
-        except requests.exceptions.Timeout as e:
+        except requests.exceptions.Timeout as exc:
             # Maybe set up for a retry, or continue in a retry loop
-            self.err_msg = self.__exception_fmt__('Timeout', e)
-        except requests.exceptions.TooManyRedirects as e:
+            self.err_msg = self.__exception_fmt__('Timeout', exc)
+        except requests.exceptions.TooManyRedirects as exc:
             # Tell the user their URL was bad and try a different one
-            self.err_msg = self.__exception_fmt__('TooManyRedirects', e)
-        except requests.exceptions.HTTPError as e:
-            self.err_msg = self.__exception_fmt__('HTTPError', e)
-        except requests.exceptions.RequestException as e:
+            self.err_msg = self.__exception_fmt__('TooManyRedirects', exc)
+        except requests.exceptions.HTTPError as exc:
+            self.err_msg = self.__exception_fmt__('HTTPError', exc)
+        except requests.exceptions.RequestException as exc:
             # catastrophic error. bail.
-            self.err_msg = self.__exception_fmt__('RequestException', e)
+            self.err_msg = self.__exception_fmt__('RequestException', exc)
         else:
             ret = resp.json()
-            # logging.debug("r.text={}".format(r.text))
+            # logging.debug("r.text=%s", r.text)
         finally:
             if self.err_msg:
                 logging.error(self.err_msg)
-            if 200 != self.status_code:
-                logging.error("dl_post failed, status_code={}".format(self.status_code))
+            if self.status_code != 200:
+                logging.error("dl_post failed, status_code=%s", self.status_code)
 
             if resp is not None:
                 self.text = resp.text
@@ -165,27 +178,34 @@ class DellinAPI(object):
         self.payload.update(self.customers_auth())
         #data = params.copy()
         #data.update(self.customers_auth())
-        #logging.info('data={}'.format(data))
+        #logging.info('data=%s'.format(data))
+
+    def dl_request_v2(self, params):
+        self.payload = params.copy()
+        self.payload.update(self.customers_auth())
+        #data = params.copy()
+        #data.update(self.customers_auth())
+        #logging.info('data=%s'.format(data))
 
         if self.session_id:
-            return self.dl_post(self.url_request)
-            # return requests.post(self.url_request, data=json.dumps(data), headers=self.headers).json()
+            return self.dl_post(self.url_request_v2)
         else:
             return self.payload
 
     def dl_test_request(self, params):
         #for k,v in params.items():
-        #    logging.info('k={}, v={}, dict={}'.format(k, v,  isinstance(v, dict)))
+        #    logging.info('k=%s, v=%s, dict=%s', k, v, isinstance(v, dict))
 
         #self.payload = params.copy()
         #self.payload.update(self.customers_auth())
         data = params.copy()
         data.update(self.customers_auth())
-        logging.info('data={}'.format(data))
+        logging.info('data=%s', data)
 
         if self.session_id:
             # return self.dl_post(self.url_request_v2)
-            return requests.post(self.url_request_v2, data=json.dumps(data), headers=self.headers).json()
+            return requests.post(self.url_request_v2,
+                                 data=json.dumps(data), headers=self.headers).json()
         else:
             return self.payload
 
@@ -220,8 +240,6 @@ class DellinAPI(object):
         self.payload.update({"name": name})
         self.payload.update({"inn": inn})
         loc_addr = '"street": "{}", "house": "{}"'.format(street_kladr, house)
-        # self.payload["juridicalAddress"] = """{{"street": "{}", "house": "{}", "building": "{}", "structure": "{}", "flat": "{}"}}""".format(\
-        #        street_kladr, house, building, structure, flat)
         if building:
             loc_addr = '{}, "building": "{}"'.format(loc_addr, building)
         if structure:
@@ -274,7 +292,11 @@ class DellinAPI(object):
         return self.dl_post(self.url_addresses_update)
 
     def dl_address_update(self, addr_id, street_kladr=None, house=None, building=None, structure=None, flat=None):
-        if street_kladr is None and house is None and building is None and structure is None and flat is None:
+        if street_kladr is None and \
+               house is None and \
+               building is None and \
+               structure is None and \
+               flat is None:
             logging.warning('Nothing to update, all arguments are None')
             return None
 
