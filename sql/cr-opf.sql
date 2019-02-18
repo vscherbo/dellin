2159cdba782157f186b8b94030d369cf45e72e1a
Vladimir Scherbo <vscherbo@gmail.com>
2019-02-18 11:02:46 +0300

    префикс и постфикс

--- sql/cr-opf.sql
+++ sql/cr-opf.sql
@@ -2,5 +2,16 @@ CREATE OR REPLACE FUNCTION arc_energo.opf(arc_energo."Предприятия")
  RETURNS character varying
  LANGUAGE sql
 AS $function$
-    SELECT trim( substring ("ЮрНазвание" from '^[а-яА-Я]+ ') ) FROM "Предприятия" where $1."Код" = "Предприятия"."Код" 
+WITH dl_opf AS (
+SELECT opf_name FROM shp.dl_opf_list WHERE 
+country_id = (SELECT c.country_id FROM shp.dl_countries c WHERE c.country_name = 'Россия')
+AND juridical) 
+    SELECT opf FROM 
+    (
+    SELECT trim('''" ' FROM substring ("ЮрНазвание" from '^[а-яА-Я]+[ "'']+') ) as opf FROM "Предприятия" where "Предприятия"."Код" = $1."Код"
+    UNION
+    SELECT trim('''" ' FROM substring ("ЮрНазвание" from '[ "'']+[а-яА-Я]+$') ) FROM "Предприятия" where "Предприятия"."Код" = $1."Код"
+    ) o
+    WHERE opf is NOT NULL and opf IN (SELECT * FROM dl_opf)
+
 $function$
