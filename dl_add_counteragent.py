4e066226aadb10852e33fe784b142513256c0952
Vladimir Scherbo <vscherbo@gmail.com>
2019-02-26 16:46:46 +0300

    проверяет входные параметры, возвращает ошибку, если некорректны

--- dl_add_counteragent.py
+++ dl_add_counteragent.py
@@ -18,6 +18,20 @@ import dl_app
 5. Добавление в таблицу dl_counteragents со статусом 1
 """
 
+ERR_REASON = {}
+ERR_REASON['name'] = 'Название'
+ERR_REASON['opf_dl'] = 'ОПФ по справочнику Деллин'
+ERR_REASON['inn'] = 'ИНН'
+ERR_REASON['street_kladr'] = 'код улицы юр. адреса по КЛАДР'
+ERR_REASON['house'] = 'номер дома'
+
+
+def ca_params_error(params):
+    """
+    returns error message for user
+    """
+    return 'Отсутствуют значения: ' + '/'.join(params)
+
 
 def main():
     """
@@ -45,31 +59,65 @@ def main():
         addr_sql = "select * from shp.dl_ca_addr_fields({})"
         curs.execute(addr_sql.format(args.code))
     (ret_flag, ret_addr_kladr_street, ret_addr_house, ret_addr_block,
-     ret_addr_flat) = curs.fetchone()
+     ret_addr_flat, ret_street) = curs.fetchone()
 
     if ret_flag:
-        """
-            with ent as (SELECT "Предприятия".opf as opf,
-            trim(replace("Предприятие", "Предприятия".opf, '')) as ent_name,
-            "ИНН" as inn FROM "Предприятия" WHERE "Код"={})
-            select ol.uid, ent.ent_name, ent.inn from ent
-            join shp.dl_opf_list ol on ent.opf = ol.opf_name
-            and country_id = (select c.country_id from shp.dl_countries c
-            where c.country_name = 'Россия');
-        """
-        sql_ent = """SELECT "Предприятия".opf_dl as opf, \
-trim(replace("Предприятие", "Предприятия".opf, '')) as ent_name, \
+        sql_ent = """SELECT "Предприятия".opf_dl as opf_dl, \
+"Предприятия".opf as opf_name, \
+trim(replace("Предприятие", coalesce("Предприятия".opf, ''), '')) as ent_name,\
+"ЮрНазвание" as legal_name, \
 "ИНН" as inn FROM "Предприятия" WHERE "Код"={};""".format(args.code)
         curs.execute(sql_ent)
-        (opf, name, inn) = curs.fetchone()
+        (opf_dl, opf_name, name, legal_name, inn) = curs.fetchone()
 
-        logging.info('app.l_book_counteragents_update(opf={}, name={}, inn={}, \
-kladr_street={}, house={}, building={}, \
-structure={}, flat={});'.format(opf, name, inn, ret_addr_kladr_street,
-                                ret_addr_house, ret_addr_block, None,
-                                ret_addr_flat))
+        logging.info('app.l_book_counteragents_update(name={}, opf_dl={},\
+opf_name={}, inn={}, \
+kladr_street={}, street={}, house={}, building={}, \
+flat={});'.format(name, opf_dl, opf_name, inn,
+                  ret_addr_kladr_street, ret_street,
+                  ret_addr_house, ret_addr_block,
+                  ret_addr_flat))
+
+        # if OK loc_status = 1
+        loc_status = int(bool(opf_dl and name and inn
+                              and ret_addr_kladr_street and ret_addr_house))
+
+        ca_params_sql = curs.mogrify(u"INSERT INTO shp.dl_ca_params\
+(arc_code, any_address,\
+ca_name, legal_name, inn, opf_dl, opf_name, \
+street_kladr, street, house, chk_result)\
+VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",
+                                     (int(args.code), args.address,
+                                      name, legal_name, inn, opf_dl, opf_name,
+                                      ret_addr_kladr_street,
+                                      ret_street,
+                                      ret_addr_house, loc_status))
+        curs.execute(ca_params_sql)
+        app.conn.commit()
+
+        # do not call dellin api
+        if loc_status != 1:
+            err_params = []
+            if name is None:
+                err_params.append(ERR_REASON['name'])
+            if opf_dl is None:
+                err_params.append('{} (ЮрНазвание: {})'.format(
+                    ERR_REASON['opf_dl'],
+                    legal_name))
+            if inn is None:
+                err_params.append(ERR_REASON['inn'])
+            if ret_addr_kladr_street is None:
+                err_params.append('{} (улица: {})'.format(
+                    ERR_REASON['street_kladr'],
+                    ret_street))
+            if ret_addr_house is None:
+                err_params.append(ERR_REASON['house'])
+            print(ca_params_error(err_params), file=sys.stderr, end='',
+                  flush=True)
+            return
+
         dl_res = app.dl.dl_book_counteragents_update(
-            opf, name, inn,
+            opf_dl, name, inn,
             street_kladr=ret_addr_kladr_street,
             house=ret_addr_house,
             building=ret_addr_block,
