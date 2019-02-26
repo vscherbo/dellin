6d6c9bbe08ccbdda5ccbeda13e555f2195b7710b
Vladimir Scherbo <vscherbo@gmail.com>
2019-02-26 16:38:56 +0300

    игнорирует скобки вокруг ОПФ

--- sql/cr-opf_dl.sql
+++ sql/cr-opf_dl.sql
@@ -5,12 +5,12 @@ AS $function$
 WITH dl_opf AS (
 SELECT opf_name, uid FROM shp.dl_opf_list WHERE 
 country_id = (SELECT c.country_id FROM shp.dl_countries c WHERE c.country_name = 'Россия')
-AND juridical) 
+AND juridical)
     SELECT uid FROM 
     (
-    SELECT trim('''" ' FROM substring ("ЮрНазвание" from '^[а-яА-Я]+[ "'']+') ) as opf FROM "Предприятия" where "Предприятия"."Код" = $1."Код"
+    SELECT trim('''" ' FROM substring (regexp_replace("ЮрНазвание", '[)(]', '', 'g') from '^[а-яА-Я]+[ "'']+') ) as opf FROM "Предприятия" where "Предприятия"."Код" = $1."Код"
     UNION
-    SELECT trim('''" ' FROM substring ("ЮрНазвание" from '[ "'']+[а-яА-Я]+$') ) FROM "Предприятия" where "Предприятия"."Код" = $1."Код"
+    SELECT trim('''" ' FROM substring (regexp_replace("ЮрНазвание", '[)(]', '', 'g') from '[ "'']+[а-яА-Я]+$') ) FROM "Предприятия" where "Предприятия"."Код" = $1."Код"
     ) o
     join dl_opf on dl_opf.opf_name = opf
     WHERE opf is NOT NULL and opf IN (SELECT opf_name FROM dl_opf)
