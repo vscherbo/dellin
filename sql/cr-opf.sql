e28e48be742607c46f2484abf24e2f83b0113d0a
Vladimir Scherbo <vscherbo@gmail.com>
2019-03-05 17:55:25 +0300

    сравнение в верхнем регистре

--- sql/cr-opf.sql
+++ sql/cr-opf.sql
@@ -3,19 +3,18 @@ CREATE OR REPLACE FUNCTION arc_energo.opf(arc_energo."Предприятия")
  LANGUAGE sql
 AS $function$
 WITH dl_opf AS (
-SELECT opf_name FROM shp.dl_opf_list WHERE 
+SELECT upper(opf_name) as opf_name FROM shp.dl_opf_list WHERE 
 country_id = (SELECT c.country_id FROM shp.dl_countries c WHERE c.country_name = 'Россия')
 AND juridical) 
     SELECT opf FROM 
     (
-     SELECT trim('''" ' FROM substring (regexp_replace("ЮрНазвание", '[)(]', '', 'g')
-            from '^[[:space:]]*[а-яА-Я]+[ "'']+') ) as opf FROM "Предприятия"
+     SELECT upper(trim('''" ' FROM substring (regexp_replace("ЮрНазвание", '[)(]', '', 'g')
+            from '^[[:space:]]*[а-яА-Я]+[ "'']+') )) as opf FROM "Предприятия"
      where "Предприятия"."Код" = $1."Код"
      UNION
-     SELECT trim('''" ' FROM substring (regexp_replace("ЮрНазвание", '[)(]', '', 'g')
-            from '[ "'']+[а-яА-Я]+[[:space:]]*$') ) FROM "Предприятия"
+     SELECT upper(trim('''" ' FROM substring (regexp_replace("ЮрНазвание", '[)(]', '', 'g')
+            from '[ "'']+[а-яА-Я]+[[:space:]]*$') )) FROM "Предприятия"
     where "Предприятия"."Код" = $1."Код"
     ) o
     WHERE opf is NOT NULL and opf IN (SELECT * FROM dl_opf)
-
 $function$
--- sql/cr-opf_dl.sql
+++ sql/cr-opf_dl.sql
@@ -3,14 +3,14 @@ CREATE OR REPLACE FUNCTION arc_energo.opf_dl(arc_energo."Предприятия"
  LANGUAGE sql
 AS $function$
 WITH dl_opf AS (
-SELECT opf_name, uid FROM shp.dl_opf_list WHERE 
+SELECT upper(opf_name) AS opf_name, uid FROM shp.dl_opf_list WHERE 
 country_id = (SELECT c.country_id FROM shp.dl_countries c WHERE c.country_name = 'Россия')
 AND juridical)
     SELECT uid FROM 
     (
-    SELECT trim('''" ' FROM substring (regexp_replace("ЮрНазвание", '[)(]', '', 'g') from '^[а-яА-Я]+[ "'']+') ) as opf FROM "Предприятия" where "Предприятия"."Код" = $1."Код"
+    SELECT upper(trim('''" ' FROM substring (regexp_replace("ЮрНазвание", '[)(]', '', 'g') from '^[а-яА-Я]+[ "'']+') )) as opf FROM "Предприятия" where "Предприятия"."Код" = $1."Код"
     UNION
-    SELECT trim('''" ' FROM substring (regexp_replace("ЮрНазвание", '[)(]', '', 'g') from '[ "'']+[а-яА-Я]+$') ) FROM "Предприятия" where "Предприятия"."Код" = $1."Код"
+    SELECT upper(trim('''" ' FROM substring (regexp_replace("ЮрНазвание", '[)(]', '', 'g') from '[ "'']+[а-яА-Я]+$') )) FROM "Предприятия" where "Предприятия"."Код" = $1."Код"
     ) o
     join dl_opf on dl_opf.opf_name = opf
     WHERE opf is NOT NULL and opf IN (SELECT opf_name FROM dl_opf)
