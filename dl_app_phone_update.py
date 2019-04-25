de2be8a11788d6d8be2a344d2861bbc85b5f5820
Vladimir Scherbo <vscherbo@gmail.com>
2019-04-25 17:38:28 +0300

    extra spaces removed

--- dl_app_phone_update.py
+++ dl_app_phone_update.py
@@ -5,9 +5,9 @@ import logging
 import sys
 import dl_app
 
-dl_app.parser.add_argument('--phone_id', type=int, required=True, help='phone_id') 
-dl_app.parser.add_argument('--phone', type=str, required=True, help='phone number') 
-dl_app.parser.add_argument('--add_num', type=str, required=False, help='extention number') 
+dl_app.parser.add_argument('--phone_id', type=int, required=True, help='phone_id')
+dl_app.parser.add_argument('--phone', type=str, required=True, help='phone number')
+dl_app.parser.add_argument('--add_num', type=str, required=False, help='extention number')
 args = dl_app.parser.parse_args()
 
 app = dl_app.DL_app(args=args, description='DL phone update')
@@ -32,7 +32,7 @@ if app.login(auth=True):
                             (args.phone, args.add_num, 1, args.phone_id ))
             else:
                 loc_sql = curs.mogrify('UPDATE ext.dl_addr_phone set phone=%s, status=%s WHERE id=%s;',
-                            (args.phone, 1, args.phone_id ))
+                            (args.phone, 1, args.phone_id))
             logging.info("loc_sql={}".format(loc_sql))
             curs.execute(loc_sql)
             app.conn.commit()
