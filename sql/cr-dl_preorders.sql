caff441b67710078cd72b982f4b2c12de68803a3
Vladimir Scherbo <vscherbo@gmail.com>
2020-10-16 12:16:22 +0300

    учитываются успешные, верифицированные и нет

--- sql/cr-dl_preorders.sql
+++ sql/cr-dl_preorders.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE FUNCTION shp.dl_preorders(OUT shp_id integer, OUT dl_dt text, 
 AS $function$
 SELECT p.shp_id, to_char(s.shipdate, 'YYYY-MM-DD'), p.req_id::text FROM shp.dl_preorder_params p
 JOIN shp.shipments s ON s.shp_id = p.shp_id AND s.carr_doc IS NULL and s.shipdate IS NOT NULL
-WHERE p.sts_code = 1; -- успешеный предзаказ и не черновик
--- WHERE p.req_id is NOT NULL;
---WHERE p.req_id = 14203091;
+WHERE p.sts_code = 1; -- успешеный предзаказ
+-- WHERE p.sts_code in (1, 10, 11); -- успешеный предзаказ, верифицирован и нет
+-- COMMENT ON COLUMN shp.dl_preorder_params.sts_code IS '0 - исходный, 1 - успешно создан, 2 - тест,черновик, 9 - сбой создания, 10 - верифицирован, 11 - есть расхождения с фактом';
 $function$
