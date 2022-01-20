202e36e01f25d05a654785a529d9b05196780f54
Vladimir Scherbo <vscherbo@gmail.com>
2022-01-20 20:27:05 +0300

    check "resp is None" if error

--- shp_dellin.py
+++ shp_dellin.py
@@ -130,7 +130,8 @@ class DellinAPI():
             if self.err_msg:
                 logging.error(self.err_msg)
                 ret = {}
-                ret = resp.json()  # v2 API
+                if resp:
+                    ret = resp.json()  # v2 API
                 ret["answer"] = {'state': 'exception', 'err_msg': self.err_msg}
             elif self.status_code != 200:
                 logging.error("dl_post failed, status_code=%s",
