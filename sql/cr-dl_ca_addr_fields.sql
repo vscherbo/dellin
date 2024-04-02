29a45abe51a8de3a2218db9b5bb68a701921d3d0
Vladimir Scherbo <vscherbo@gmail.com>
2024-04-02 13:44:41 +0300

    по равенству улицы, не LIKE

--- sql/cr-dl_ca_addr_fields.sql
+++ sql/cr-dl_ca_addr_fields.sql
@@ -1,4 +1,5 @@
-drop function shp.dl_ca_addr_fields(integer, varchar);
+--drop function shp.dl_ca_addr_fields(integer, varchar);
+
 CREATE OR REPLACE FUNCTION shp.dl_ca_addr_fields(arg_code integer,
     arg_addr_text varchar DEFAULT NULL,
 out ret_flg boolean,
@@ -14,17 +15,21 @@ out ret_addr_city varchar
  RETURNS record
  LANGUAGE plpgsql
 AS $function$
+DECLARE
+    street_with_type varchar;
 begin
-    SELECT * FROM dadata_address(arg_code, arg_addr_text)
-    into ret_flg, ret_addr_city, ret_addr_city_code, ret_addr_street, ret_addr_street_type, ret_addr_house, ret_addr_block, ret_addr_flat ;
+    SELECT * FROM dadata_address_ext(arg_code, arg_addr_text)
+    into ret_flg, ret_addr_city, ret_addr_city_code, ret_addr_street, ret_addr_street_type, street_with_type, ret_addr_house, ret_addr_block, ret_addr_flat ;
 
     IF ret_addr_street IS NULL THEN
         ret_addr_street := '';
         ret_addr_street_type := '';
     ELSE
+        RAISE NOTICE 'ret_addr_street=%, ret_addr_street_type=%, street_with_type=[%]', ret_addr_street, ret_addr_street_type, street_with_type;
         SELECT street_code into ret_street_code FROM ext.dl_streets ds 
-        -- where ds.search_string = replace(ret_addr_street, 'ё', 'е')
-        where ds.search_string ILIKE '%' || replace(ret_addr_street, 'ё', 'е') || '%'
+        -- OLD where ds.search_string ILIKE '%' || replace(ret_addr_street, 'ё', 'е') || '%'
+        -- where ds.street_name ILIKE '%' || replace(street_with_type, 'ё', 'е') || '%'
+        where ds.street_name = street_with_type
         AND ds.city_id IN (SELECT dp.city_id FROM ext.dl_places dp WHERE dp.code LIKE ret_addr_city_code || '%');
     END IF;
 
