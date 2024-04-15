9404257e51bb805d60c8d9f992f0627fd44cb198
Vladimir Scherbo <vscherbo@gmail.com>
2024-04-15 12:11:25 +0300

    right way for --test arg

--- sql/cr-dl_request_v2.sql
+++ sql/cr-dl_request_v2.sql
@@ -10,17 +10,16 @@ DECLARE cmd character varying;
   wrk_dir text := '/opt/dellin';
   loc_app_srv varchar;
 BEGIN
-    cmd := format('python3 %s/dl_app_req.py --log_file=%s/dl_app_req_v2.log --conf=%s --shp_id=%s --test=%s', 
+    cmd := format('python3 %s/dl_app_req.py --log_file=%s/dl_app_req_v2.log --conf=%s --shp_id=%s', 
         wrk_dir, -- script dir
         wrk_dir, -- logfile dir
         format('%s/dl.conf', wrk_dir), -- conf file
-        arg_shp_id,
-        arg_test);
+        arg_shp_id);
 
     RAISE NOTICE 'dl_request_v2, test=%', arg_test;
-    --IF arg_test THEN
-        --cmd := format('%s --test=True', cmd);
-    --END IF;
+    IF arg_test THEN
+        cmd := format('%s --test=True', cmd);
+    END IF;
 
     IF cmd IS NULL 
     THEN 
