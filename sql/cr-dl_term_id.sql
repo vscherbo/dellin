74613349c145edba38b34ee534d19d55f15f8221
Владимир Щербо <vscherbo@gmail.com>
2024-12-02 21:07:55 +0300

    term_id from vw if not found

--- sql/cr-dl_term_id.sql
+++ sql/cr-dl_term_id.sql
@@ -2,16 +2,21 @@
 
 CREATE OR REPLACE FUNCTION shp.dl_term_id(arg_addr_id integer)
  RETURNS integer
- LANGUAGE sql
+ LANGUAGE plpgsql
 AS $function$
-SELECT terminal_id AS RESULT FROM ext.dl_addresses WHERE id=arg_addr_id
-UNION 
-SELECT terminal_id FROM shp.vw_dl_addresses WHERE id=arg_addr_id;
-$function$
+DECLARE
+loc_res integer;
+BEGIN
+SELECT terminal_id INTO loc_res FROM ext.dl_addresses WHERE id=arg_addr_id;
+IF NOT FOUND THEN
+  SELECT terminal_id INTO loc_res FROM shp.vw_dl_addresses WHERE id=arg_addr_id;
+END IF;
+RETURN loc_res;
+END;$function$
 ;
 
 -- Permissions
 
 ALTER FUNCTION shp.dl_term_id(int4) OWNER TO arc_energo;
+GRANT ALL ON FUNCTION shp.dl_term_id(int4) TO public;
 GRANT ALL ON FUNCTION shp.dl_term_id(int4) TO arc_energo;
-
